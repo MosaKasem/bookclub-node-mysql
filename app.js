0a3a8710b6f65ca0fb6300d2030741520ad37ac3
MosaKasem <thescorpion119@gmail.com>
2019-01-08 14:53:18 +0100

    setting up express

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const mysql = require('mysql')
 const exphbs = require('express-handlebars')
+const bodyparser = require('body-parser')
+
+const path = require('path')
 
 const app = express()
 
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  partialsDir: __dirname + '/views/flash'
+}))
+
+app.set('view engine', 'handlebars')
+/* app.use((req, res, next) => {
+  // if (req.session.flash !== undefined) {
+  res.locals.flash = req.session.flash
+  delete req.session.flash
+  // }
+  console.log(req.session.flash)
+  next()
+}) */
+
 app.listen('3000', () => {
-  console.log('App is running')
+  console.log('running')
 })
