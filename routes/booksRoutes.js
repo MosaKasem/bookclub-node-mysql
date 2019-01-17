5815994713fc52eed281791d306216d5cd8db8e5
MosaKasem <thescorpion119@gmail.com>
2019-01-17 01:17:01 +0100

    l3na 3la abokom

--- routes/booksRoutes.js
+++ routes/booksRoutes.js
@@ -5,48 +5,57 @@ router.use(bodyParser.urlencoded({extended: true}))
 
 const db = require('../model/dbConnect')
 db.connect((err) => {
-  if (err) console.log(err)
-  console.log('connected to database')
+  if (err) throw err
+  console.log('connected to phpmyadmin')
 })
-function executeQuery (query) {
-  new Promise(async (resolve, reject) => {
-    db.query(query, (err, result) => {
-      if (err) console.log(err)
-      resolve(result)
-    })
+
+const getAllBooks = () => {
+  new Promise(async resolve => {
+    const books = await executeQuery('SELECT * FROM books')
+    console.log('books: ', books)
+    resolve(books)
   })
 }
-const getAllBooks = () => {
-  new Promise(async (resolve, reject) => {
-    const books = await executeQuery(`
-    SELECT * FROM books`
-  )
-    resolve(books)
+const executeQuery = (query) => {
+  let data = {}
+  new Promise((resolve) => {
+    db.query(query, function (err, result) {
+      if (err) throw err
+      data = {
+        id: result.id,
+        title: result.title,
+        description: result.description,
+        bookinfo: result.bookinfo,
+        author: result.author,
+        published: result.published
+      }
+    })
+    console.log(data)
+    resolve(data)
+/*     await db.query(query, (err, result) => {
+      if (err) throw err
+      return resolve(result)
+    }) */
   })
 }
 
 /* const db = database.connect((err) => {
-  if (err) console.log(err)
-  console.log('connected to database')
+  if (err)
+
 }) */
 
-router.route('/books').get(async (req, res) => {
-  let books = await getAllBooks()
-  const allbooks = {
-    everybook: books.map((book) => {
-      return {
-        title: book.title
-      }
-    })
-  }
-  res.render('books/everybook', {allbooks})
-})
+router.route('/books')
+  .get(async (req, res) => {
+    let books = await getAllBooks()
+    console.log('books: ', books)
+    res.render('books/everybook', {books})
+  })
 
 router.route('/createDB').get((req, res) => {
   let sql = 'CREATE DATABASE IF NOT EXISTS dbForBookAssignmentThree'
   db.query(sql, (err, result) => {
     if (err) throw err
-    console.log(result)
+
     res.send('database created')
   })
 })
@@ -63,8 +72,7 @@ router.route('/createTable').get((req, res) => {
     )`
   let sql2 = `CREATE TABLE IF NOT EXISTS bookcategory()`
   db.query(`${sql}; ${sql2}`, (err, result) => {
-    if (err) console.log(err)
-    console.log(result)
+    if (err) throw err
     res.send('post table created')
   })
 })
@@ -73,25 +81,21 @@ router.route('/addpost').get((req, res) => {
   let post = {title: 'maximus', description: 'Jimmyswagfiftyshades', bookinfo: 'sweden', author: 'JohnSnow', published: 1655}
   let sql = 'INSERT INTO books SET ?'
   let query = db.query(sql, post, (err, result) => {
-    if (err) console.log(err)
+    if (err) throw err
     res.send('post added')
   })
 })
 
 router.route('/selectPost/:weee').get((req, res) => {
-  console.log(req.params.weee)
   let sql = 'SELECT * FROM books'
   let query = db.query(sql, (err, results) => {
-    if (err) console.log(err)
-    console.log(results)
+    if (err) throw err
   })
 })
 router.route('/selectPostUrl/:id').get((req, res) => {
-  console.log('\n')
   let sql = `SELECT * FROM books WHERE author="${req.params.id}"`
   let query = db.query(sql, (err, results) => {
-    if (err) console.log(err)
-    console.log(results)
+    if (err) throw err
   })
 })
 
